922c91f642ceae730e973523263185f9372e37de
luvinhthinh <luvinhthinh.mono@gmail.com>
2016-01-19 06:28:49 +0800

    Create where.js

---
+++ week4/where.js
@@ -0,0 +1,11 @@
+myData = [
+[-31.9484728,115.8603705, 'Cpp State Library Carpark, Perth WA 6000, Australia'],
+[-35.7752426,137.2142417, 'Kangaroo Island, South Australia, Australia'],
+[14.6117355,121.1007119, 'Adelaide, Pasig, Metro Manila, Philippines'],
+[-34.9228309,138.6025614, 'Rundle Mall, Adelaide SA 5000, Australia'],
+[-34.9296417,138.5964562, 'Central Market, Adelaide SA 5000, Australia'],
+[42.340075,-71.0895367, 'Northeastern, Boston, MA 02115, USA'],
+[38.2113643,-85.7470011, 'Bradley Ave, Louisville, KY, USA'],
+[32.778949,35.019648, 'Technion/ Sports Building, Haifa'],
+[18.4574518,73.8837999, 'Vishwakarma Institutes Play Ground, Yashodhan Society, Kapil Nagar, Kondhwa Budrukh, Vishwakarma, Maharashtra 411048, India']
+];
